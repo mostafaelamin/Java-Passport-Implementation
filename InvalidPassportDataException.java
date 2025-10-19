7814c28cd76366156c3a7b32f2734390d42fba09
Mostafa Elamin <melamin1@terpmail.umd.edu>
2025-10-18 23:22:39 -0400

    Update InvalidPassportDataException.java

--- InvalidPassportDataException.java
+++ InvalidPassportDataException.java
@@ -1,1 +1,15 @@
+package com.passport.system;
 
+/**
+ * Custom exception for handling invalid data when creating or updating a Passport.
+ */
+public class InvalidPassportDataException extends Exception {
+
+    public InvalidPassportDataException(String message) {
+        super(message);
+    }
+
+    public InvalidPassportDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
